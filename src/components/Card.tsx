934ff707ac529cd1f8cc1b7cc914d39d3403ae59
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-12-13 05:35:27 -0300

    updating to react-query

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import * as Dialog from '@radix-ui/react-dialog'
+import { api } from '../lib/axios'
+import { useQuery } from 'react-query'
 
 type PokemonCardProps = {
   name: string
@@ -9,127 +8,91 @@ type PokemonCardProps = {
 }
 
 type PokemonProps = {
-  id: number
   name: string
-  base_experience: number
-  height: number
-  order: number
-  weight: number
-  url: string
-  abilities: string[]
-  types: string[]
+  id: number
+  sprites: { front_default: string }
 }
 
 export function PokemonCard({ name, url }: PokemonCardProps) {
-  const [pokemon, setPokemon] = useState({} as PokemonProps)
-  const { abilities, types } = pokemon
-
-  useEffect(() => {
-    async function getPokemonData() {
-      const res = await axios.get(url)
-      const data = res.data
-
-      const abilities = data.abilities.map(
-        (item: { ability: { name: string } }) => {
-          return item.ability.name
-        },
-      )
-      const types = data.types.map((item: { type: { name: string } }) => {
-        return item.type.name
-      })
-      console.log(data)
-
-      const poke: PokemonProps = {
-        id: data.id,
-        name: data.name,
-        base_experience: data.base_experience,
-        height: data.height,
-        order: data.order,
-        weight: data.weight,
-        url: data.sprites.front_default,
-        abilities,
-        types,
-      }
-      setPokemon(poke)
-    }
-    getPokemonData()
-  }, [url])
+  const { data: fetchPokemon } = useQuery<PokemonProps>(
+    ['pokemon', url],
+    async () => {
+      const { data } = await api.get(url)
+      return data
+    },
+  )
 
   return (
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <div className="flex flex-col gap-4 px-6 py-4 w-48 items-center justify-center rounded-xl bg-purple-500 text-white font-bold">
-          <div className="-mt-12">
-            <Image src={pokemon.url} alt="" width={100} height={100} />
-          </div>
-          <div className="flex gap-2 w-full justify-between rounded-lg px-2 py-1 bg-gray-600">
-            <span className="text-purple-300 flex-1">#{pokemon.id}</span>
-            <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">
-              {name}
-            </h1>
+    <div>
+      <div className="flex flex-col gap-4 px-6 py-4 w-48 items-center justify-center rounded-xl bg-purple-500 text-white font-bold">
+        <div className="-mt-12">
+          <Image
+            src={fetchPokemon?.sprites.front_default}
+            alt="pokemon image"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="flex gap-2 w-full justify-between rounded-lg px-2 py-1 bg-gray-600">
+          <span className="text-purple-300 flex-1">#{fetchPokemon?.id}</span>
+          <h1 className="overflow-hidden whitespace-nowrap overflow-ellipsis">
+            {name}
+          </h1>
+        </div>
+      </div>
+    </div>
+    /* <div className="flex flex-col items-center justify-center gap-8 w-2/3 max-w-md rounded-xl bg-white shadow-2xl border border-gray-500 fixed top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%]">
+        <div className="flex flex-col gap-4">
+          <Image
+            alt="image pokemon"
+            src={fetchPokemon?.url}
+            width={150}
+            height={100}
+          />
+          <div className="flex justify-between">
+            {types?.map((item) => (
+              <div
+                key={item}
+                className="py-2 px-3 text-center text-white bg-blue-600 rounded"
+              >
+                {item}
+              </div>
+            ))}
           </div>
         </div>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="fixed" />
-        <Dialog.Content className="flex flex-col items-center justify-center gap-8 w-2/3 max-w-md rounded-xl bg-white shadow-2xl border border-gray-500 fixed top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%]">
-          <Dialog.Title className="flex flex-col gap-4">
-            <Image
-              alt="image pokemon"
-              src={pokemon.url}
-              width={150}
-              height={100}
-            />
-            <div className="flex justify-between">
-              {types?.map((item) => (
-                <div
-                  key={item}
-                  className="py-2 px-3 text-center text-white bg-blue-600 rounded"
-                >
-                  {item}
-                </div>
-              ))}
+        <div className="grid grid-cols-1 gap-2 bg-purple-500 px-14 py-6 w-full h-full rounded-t-3xl sm:grid-cols-2 md:grid-cols-3">
+          <div>
+            <div className="flex flex-col text-white">
+              Height
+              <p className="text-gray-300">{fetchPokemon?.height}</p>
             </div>
-          </Dialog.Title>
-          <Dialog.Description className="grid grid-cols-1 gap-2 bg-purple-500 px-14 py-6 w-full h-full rounded-t-3xl sm:grid-cols-2 md:grid-cols-3">
-            <div>
-              <div className="flex flex-col text-white">
-                Height
-                <p className="text-gray-300">{pokemon.height}</p>
-              </div>
-              <div className="flex flex-col text-white">
-                Weight
-                <p className="text-gray-300">{pokemon.weight}</p>
-              </div>
-              <div className="flex flex-col text-white">
-                Order
-                <p className="text-gray-300">{pokemon.order}</p>
-              </div>
+            <div className="flex flex-col text-white">
+              Weight
+              <p className="text-gray-300">{fetchPokemon?.weight}</p>
             </div>
-            <div>
-              <div className="flex flex-col text-white">
-                Base Exp.
-                <p className="text-gray-300">{pokemon.base_experience}</p>
-              </div>
+            <div className="flex flex-col text-white">
+              Order
+              <p className="text-gray-300">{fetchPokemon?.order}</p>
             </div>
-            <div>
-              <div>
-                <div className="flex flex-col gap-2 text-white">
-                  <p className="font-semibold">Abilities</p>
-                  {abilities?.map((item) => (
-                    <p
-                      key={item}
-                      className="bg-green-500 px-2 py-1 w-full max-w-[10rem] rounded text-center"
-                    >
-                      {item}
-                    </p>
-                  ))}
-                </div>
-              </div>
+          </div>
+          <div>
+            <div className="flex flex-col text-white">
+              Base Exp.
+              <p className="text-gray-300">{fetchPokemon?.base_experience}</p>
             </div>
-          </Dialog.Description>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+          </div>
+          <div className="flex flex-col gap-2 text-white">
+            <p className="font-semibold">Abilities</p>
+            {abilities?.map((item) => (
+              <p
+                key={item}
+                className="bg-green-500 px-2 py-1 w-full max-w-[10rem] rounded text-center"
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div> */
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,49 @@
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useQuery } from 'react-query'
 import Logo from '../assets/Logo.png'
 import { PokemonCard } from '../components/Card'
 import { api } from '../lib/axios'
 
-type PokemonsProps = {
-  name: string
-  url: string
+type urlProps = {
+  next: string | null
+  previous: string | null
+  pokemons: {
+    name: string
+    url: string
+  }[]
 }
 
 export default function Home() {
-  const [urls, setUrls] = useState<PokemonsProps[]>([])
   const [nextPage, setNextPage] = useState('')
   const [previousPage, setPreviousPage] = useState('')
 
-  async function getPokemonsUrl() {
-    const response = await api.get('pokemon')
-    const results = response.data.results
-    const nextPages = response.data.next
-    const previousPages = response.data.previous
-    setUrls(results)
-    setNextPage(nextPages)
-    setPreviousPage(previousPages)
-  }
+  const { data: fetchUrls } = useQuery<urlProps>('urls', async () => {
+    const { data } = await api.get('pokemon')
+    const urlData: urlProps = {
+      next: data.next,
+      previous: data.previous,
+      pokemons: data.results,
+    }
+
+    return urlData
+  })
 
   async function NextPage() {
     const response = await api.get(`${nextPage}`)
-    const results = response.data.results
     const nextPages = response.data.next
     const previousPages = response.data.previous
-    setUrls(results)
     setNextPage(nextPages)
     setPreviousPage(previousPages)
   }
 
   async function PreviousPage() {
     const response = await api.get(`${previousPage}`)
-    const results = response.data.results
     const nextPages = response.data.next
     const previousPages = response.data.previous
-    setUrls(results)
     setNextPage(nextPages)
     setPreviousPage(previousPages)
   }
-
-  useEffect(() => {
-    getPokemonsUrl()
-  }, [])
 
   return (
     <div className="flex flex-col gap-12 items-center max-w-2xl m-auto bg-white">
@@ -54,8 +51,10 @@ export default function Home() {
         <Image src={Logo} alt="" />
       </div>
       <div className="flex flex-wrap justify-center items-center gap-x-4 gap-y-14">
-        {urls.map((item) => {
-          return <PokemonCard key={item.name} url={item.url} name={item.name} />
+        {fetchUrls?.pokemons.map((props) => {
+          return (
+            <PokemonCard key={props.name} name={props.name} url={props.url} />
+          )
         })}
       </div>
       <div className="flex gap-4 mb-8">
